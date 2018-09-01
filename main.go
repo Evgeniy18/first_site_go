3821c5f24d89e131cf3ec80ff64128277a741f64
Evgeniy <zhekab18@gmail.com>
2018-09-01 22:31:32 +0300

    1

--- main.go
+++ main.go
@@ -2,17 +2,19 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Привет, мир!")
-	w.Write([]byte("!!!"))
+	io.WriteString(w, "Hello, World!")
 }
 
 func main() {
+	port := os.Getenv("PORT")
 	http.HandleFunc("/", handler)
 
 	fmt.Println("starting server at :8080")
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":"+port, nil)
 }
