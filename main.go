dc684457c3b0857122bce67d7b558a817194f1c2
Evgeniy <zhekab18@gmail.com>
2018-09-04 21:13:14 +0300

    5

--- main.go
+++ main.go
@@ -3,19 +3,14 @@ package main
 import (
 	"fmt"
 	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Привет, мир!")
+	w.Write([]byte("!!!"))
+}
 func main() {
-
-	router := gin.New()
-	router.Use(gin.Logger())
-
-	router.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Hello, World!", nil)
-	})
-
-	fmt.Println(":8080")
-	router.Run(":8080")
+	http.HandleFunc("/", handler)
+	fmt.Println("starting server at :8080")
+	http.ListenAndServe(":8080", nil)
 }
