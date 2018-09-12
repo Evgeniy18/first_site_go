d076bdc70331c100c2cff20560e85a5a7f059e87
Evgeniy <zhekab18@gmail.com>
2018-09-12 16:44:59 +0300

    add stops_pretty

--- main.go
+++ main.go
@@ -7,8 +7,17 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func stops(w http.ResponseWriter, r *http.Request) {
 	f, err := ioutil.ReadFile("src/stops.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write(f)
+}
+
+func stopsPretty(w http.ResponseWriter, r *http.Request) {
+	f, err := ioutil.ReadFile("src/stops_pretty.json")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,7 +33,8 @@ func main() {
 		log.Fatal("$PORT must be set")
 	}
 
-	http.HandleFunc("/stops", handler)
+	http.HandleFunc("/stops", stops)
+	http.HandleFunc("/stops_pretty", stopsPretty)
 	http.ListenAndServe(":"+port, nil)
 
 }
