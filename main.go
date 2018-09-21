9d22a19060f6a6298257faf70a6a8c5082ceffdc
Evgeniy <zhekab18@gmail.com>
2018-09-21 16:57:53 +0300

    change port

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
 var files = [11]string{
@@ -202,11 +203,11 @@ func RTFeed(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	/* port := os.Getenv("PORT")
+	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("$PORT must be set")
-	} */
-	port := "8080"
+	}
+	//port := "8080"
 
 	fs := http.FileServer(http.Dir("static"))
 
