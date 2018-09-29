a577a04371038c871515d9386b2cdddff0c3cad2
Evgeniy <zhekab18@gmail.com>
2018-09-29 15:35:07 +0300

    always updating

--- main.go
+++ main.go
@@ -421,29 +421,32 @@ func saveXMLToJSONWithStruct(i *Siri, out string) {
 	}
 	defer resp.Body.Close()
 
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
+	log.Print(resp.StatusCode)
+	if resp.StatusCode == 200 {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	err = xml.Unmarshal(body, i)
-	if err != nil {
-		log.Fatal(err)
-	}
+		err = xml.Unmarshal(body, i)
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	//newService := improveServiceStatus(*i)
-	newService := improveServiceStatusSubway(*i)
-	json, err := json.MarshalIndent(newService, "", "	")
-	if err != nil {
-		log.Fatal(err)
+		//newService := improveServiceStatus(*i)
+		newService := improveServiceStatusSubway(*i)
+		json, err := json.MarshalIndent(newService, "", "	")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fout, err := os.Create(out)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer fout.Close()
+		fmt.Fprintf(bufio.NewWriter(fout), "%s", string(json))
+		log.Print("File was refreshed.")
 	}
-	fout, err := os.Create(out)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer fout.Close()
-	fmt.Fprintf(bufio.NewWriter(fout), "%s", string(json))
-	log.Print("File was refreshed.")
 }
 
 func improveServiceStatusSubway(s Siri) []Situation {
@@ -488,10 +491,10 @@ func improveServiceStatusSubway(s Siri) []Situation {
 
 func updateFile() {
 	for {
+		time.Sleep(90 * time.Second)
 		myStruct := &Siri{}
 		fileNameOut := "src/improvedServiceStatusSubway.json"
 		saveXMLToJSONWithStruct(myStruct, fileNameOut)
-		time.Sleep(1 * time.Minute)
 	}
 }
 
